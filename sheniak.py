ef7e1f821b5a45bc85d2598a63046182fc9f39e2
Keith Hamilton <the.keith.hamilton@gmail.com>
2017-05-10 14:27:49 +0000

    Update

--- sheniak.py
+++ sheniak.py
@@ -1,7 +1,7 @@
 import random
 
 import gpiozero
-from sh import afplay
+from sh import mpg321
 
 AUDIO_FILES = [
     './assets/audio/laugh_1.mp3',
@@ -12,10 +12,10 @@ AUDIO_FILES = [
     './assets/audio/laugh_6_short.mp3',
     './assets/audio/laugh_7.mp3'
     ]
-PIR = gpiozero.MotionSensor(4)
+PIR = gpiozero.MotionSensor(14, queue_len=10, threshold=0.999)
 
 while True:
-    if PIR.motion_detected:
-        print("Motion detected!")
-        laugh_track = random.choice(AUDIO_FILES)
-        afplay(laugh_track)
+    PIR.wait_for_motion()
+    print("Motion detected!")
+    laugh_track = random.choice(AUDIO_FILES)
+    mpg321(laugh_track)
